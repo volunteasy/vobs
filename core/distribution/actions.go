15489412cdc5229fdf819dfcbf835eb3a8040de4
Igor <igor.reisleandro@gmail.com>
2023-03-14 21:44:15 -0300

    feat: add distribution actions

--- core/distribution/actions.go
+++ core/distribution/actions.go
@@ -1,1 +1,13 @@
 package distribution
+
+import (
+	"context"
+    "govobs/core/types"
+)
+
+type Actions interface {
+	CreateDistribution(ctx context.Context, d Distribution) error
+	ListDistributions(ctx context.Context, f Filter) ([]Distribution, int, error)
+	GetDistribution(ctx context.Context, id types.ID) (Distribution, error)
+	UpdateDistribution(ctx context.Context, distribution Distribution) error
+}
