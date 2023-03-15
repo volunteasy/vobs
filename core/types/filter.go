1f1834d94e49cb6059d01bb6ad5a6b69897d7ad6
Igor <igor.reisleandro@gmail.com>
2023-03-14 21:25:31 -0300

    feat: date range added as a value object

--- core/types/filter.go
+++ core/types/filter.go
@@ -1,4 +1,32 @@
 package types
 
+import (
+    "errors"
+    "time"
+)
+
+var (
+	ErrInvalidDateRange = errors.New("end date must be equal or after start date")
+)
+
 type Filter struct {
+}
+
+
+type DateRange struct {
+	Start time.Time
+	End time.Time
+}
+
+func (f Filter) Validate() error {
+	return nil
+}
+
+
+func (d DateRange) Validate() error {
+	if d.End.Before(d.Start) {
+		return ErrInvalidDateRange
+	}
+
+	return nil
 }
