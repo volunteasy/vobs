c64e0b0d42cb77ffe8be7cf332d079b29b99280b
Igor <igor.reisleandro@gmail.com>
2023-03-15 23:22:45 -0300

    feat: add test endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,15 @@
 package main
 
+import "net/http"
 
 func main() {
-	println("ni hao!")
+
+	http.HandleFunc("/health", func(writer http.ResponseWriter, request *http.Request) {
+		println("ni hao!")
+    })
+
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        panic(err)
+    }
 }
