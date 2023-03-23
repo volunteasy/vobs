18d142e1dec663e4966ea720ada08af4a3774bb8
Igor <igor.reisleandro@gmail.com>
2023-03-23 18:26:59 -0300

    feat: general changes

--- providers/sql/conn.go
+++ providers/sql/conn.go
@@ -15,20 +15,13 @@ import (
 )
 
 func NewConnection(c config.MySQL) (*sql.DB, func() error, error) {
-	var cfg *mysql.Config
-	var err error
-
-	if c.DSN == "" {
+	cfg, err := mysql.ParseDSN(c.DSN)
+	if err != nil {
 		cfg := mysql.NewConfig()
 		cfg.Addr = c.Host
 		cfg.User = c.User
 		cfg.Passwd = c.Password
 		cfg.DBName = c.Name
-	} else {
-		cfg, err = mysql.ParseDSN(c.DSN)
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed parsing DSN: %w", err)
-		}
 	}
 
 	driver, err := mysql.NewConnector(cfg)
