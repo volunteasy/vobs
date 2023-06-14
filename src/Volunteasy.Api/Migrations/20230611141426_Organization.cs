f188def125411cd27df24743fc7a1186f2f55c41
eakeur <igor.reisleandro@gmail.com>
2023-06-14 15:28:12 -0300

    feat: faltou as migs

---
+++ src/Volunteasy.Api/Migrations/20230611141426_Organization.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Volunteasy.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Organization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Document = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: false),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CoordinateX = table.Column<float>(type: "real", nullable: false),
+                    CoordinateY = table.Column<float>(type: "real", nullable: false),
+                    AddressName = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Memberships",
+                columns: table => new
+                {
+                    OrganizationId = table.Column<long>(type: "bigint", nullable: false),
+                    MemberId = table.Column<long>(type: "bigint", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    MemberSince = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memberships", x => new { x.MemberId, x.OrganizationId });
+                    table.ForeignKey(
+                        name: "FK_Memberships_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_OrganizationId",
+                table: "Memberships",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_Role",
+                table: "Memberships",
+                column: "Role");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_Status",
+                table: "Memberships",
+                column: "Status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Organizations_Document",
+                table: "Organizations",
+                column: "Document",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Memberships");
+
+            migrationBuilder.DropTable(
+                name: "Organizations");
+        }
+    }
+}
