0329e4e3031ade55a888b8be53f6a450df2be549
eakeur <igor.reisleandro@gmail.com>
2023-07-07 21:27:15 -0300

    f

--- src/Volunteasy.Web/Auth/AuthenticatedUserData.cs
+++ src/Volunteasy.Web/Auth/AuthenticatedUserData.cs
@@ -1,41 +1,18 @@
-using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using FirebaseAdmin.Auth;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.IdentityModel.Tokens;
 using Volunteasy.Core.Enums;
-using Volunteasy.Core.Model;
-using Volunteasy.Core.Services;
 using ISession = Volunteasy.Core.Services.ISession;
 
 namespace Volunteasy.Web.Auth;
 
 public class SessionProvider : AuthenticationStateProvider, ISession
 {
-
-    private readonly IIdentityService _identity;
-
-    private readonly FirebaseAuth _firebase;
-
-    public SessionProvider(IIdentityService identity, FirebaseAuth firebase)
-    {
-        _identity = identity;
-        _firebase = firebase;
-    }
-    
     private ClaimsPrincipal User { get; set; } = new ();
     
     public override Task<AuthenticationState> GetAuthenticationStateAsync()
     {
         return Task.FromResult(new AuthenticationState(User));
-    }
-
-    public static (long, long) GetFromState(AuthenticationState state)
-    {
-        return (
-            Convert.ToInt64(state.User.FindFirst("volunteasy_id")?.Value),
-            Convert.ToInt64(state.User.FindFirst("organization_id")?.Value));
     }
 
     public long UserId => Convert.ToInt64(User.FindFirst("volunteasy_id")?.Value);
@@ -57,34 +34,4 @@ public class SessionProvider : AuthenticationStateProvider, ISession
     }
 
     public MembershipRole CurrentRole => MembershipRole.Owner;
-    
-    public async Task AuthenticateUser(string email, string password)
-    {
-
-        try
-        {
-            var token = await _identity.AuthenticateUser(new UserCredentials
-            {
-                Email = email, Password = password
-            });
-
-            var claims = await _firebase.VerifyIdTokenAsync(token);
-            if (claims == null)
-                throw new Exception("no claim");
-            
-            var userId = Convert.ToInt64(claims.Claims["volunteasy_id"] ?? "volunteasy_id");
-            
-            User = new ClaimsPrincipal(
-                new ClaimsIdentity(await _identity.GetUserSessionClaims2(userId), JwtBearerDefaults.AuthenticationScheme));
-
-            NotifyAuthenticationStateChanged(
-                Task.FromResult(new AuthenticationState(User)));
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            User = new ClaimsPrincipal();
-        }
-        
-    }
 }
--- src/Volunteasy.Web/Program.cs
+++ src/Volunteasy.Web/Program.cs
@@ -1,70 +1,67 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Text;
-using System.Text.Json.Serialization;
 using dotenv.net;
 using FirebaseAdmin;
 using FirebaseAdmin.Auth;
 using Google.Apis.Auth.OAuth2;
 using IdGen.DependencyInjection;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Logging;
-using Microsoft.IdentityModel.Tokens;
 using Volunteasy.Application;
 using Volunteasy.Application.Services;
 using Volunteasy.Core.Data;
 using Volunteasy.Core.Services;
 using Volunteasy.Infrastructure.Firebase;
 using Volunteasy.Web.Auth;
-using Volunteasy.Web.Data;
 using ISession = Volunteasy.Core.Services.ISession;
 
 var builder = WebApplication.CreateBuilder(args);
 
-    // This is meant to load env vars in a local environment.
-    // Production and Staging environments should not depend on this
-    if (builder.Environment.IsDevelopment())
-        DotEnv.Load(options: new DotEnvOptions(ignoreExceptions: false));
+// This is meant to load env vars in a local environment.
+// Production and Staging environments should not depend on this
+if (builder.Environment.IsDevelopment())
+    DotEnv.Load(options: new DotEnvOptions(ignoreExceptions: false));
 
 builder.Configuration.AddEnvironmentVariables();
 
-    builder.Services.AddIdGen(123);
+builder.Services.AddIdGen(123);
 
-    builder.Services.AddDbContext<Data>(o => o.UseNpgsql(
-            builder.Configuration.GetValue<string>("POSTGRES_CONNSTR") ?? "",
-            op => op.MigrationsAssembly("Volunteasy.Api")
-        )
-    );
+builder.Services.AddDbContext<Data>(o => o.UseNpgsql(
+        builder.Configuration.GetValue<string>("POSTGRES_CONNSTR") ?? "",
+        op => op.MigrationsAssembly("Volunteasy.Api")
+    )
+);
 
-    var firebaseCredentials = builder.Configuration.GetValue<string>("FIREBASE_CREDS");
-    var firebaseSignIn = builder.Configuration.GetValue<string>("FIREBASE_SIGNIN_URL");
-    var fb = FirebaseAuth.GetAuth(FirebaseApp.Create(new AppOptions
+var firebaseCredentials = builder.Configuration.GetValue<string>("FIREBASE_CREDS");
+var firebaseSignIn = builder.Configuration.GetValue<string>("FIREBASE_SIGNIN_URL");
+var fb = FirebaseAuth.GetAuth(FirebaseApp.Create(new AppOptions
+{
+    Credential = GoogleCredential.FromJson(firebaseCredentials)
+}));
+
+builder.Services.AddSingleton(fb);
+builder.Services.AddScoped<IAuthenticator>(b => new Auth(
+    fb, b.GetService<ILogger<Auth>>()!, firebaseSignIn ?? ""));
+
+builder.Services.AddScoped<ISession, SessionProvider>();
+builder.Services.AddScoped<IIdentityService, IdentityService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IOrganizationService, OrganizationService>();
+builder.Services.AddScoped<IMembershipService, MembershipService>();
+// builder.Services.AddScoped<IDistributionService, DistributionService>();
+// builder.Services.AddScoped<IBenefitService, BenefitService>();
+// builder.Services.AddScoped<IBenefitProvisionService, BenefitProvisionService>();
+// builder.Services.AddScoped<IBenefitItemService, BenefitItemService>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
     {
-        Credential = GoogleCredential.FromJson(firebaseCredentials)
-    }));
+        options.LoginPath = "/admin/auth";
+    });
 
-    builder.Services.AddSingleton(fb);
-    builder.Services.AddScoped<IAuthenticator>(b => new Auth(
-        fb, b.GetService<ILogger<Auth>>()!, firebaseSignIn ?? ""));
-
-    builder.Services.AddScoped<ISession, SessionProvider>();
-    builder.Services.AddScoped<AuthenticationStateProvider, SessionProvider>();
-    builder.Services.AddScoped<IIdentityService, IdentityService>();
-    builder.Services.AddScoped<IUserService, UserService>();
-    builder.Services.AddScoped<IOrganizationService, OrganizationService>();
-    builder.Services.AddScoped<IMembershipService, MembershipService>();
-    // builder.Services.AddScoped<IDistributionService, DistributionService>();
-    // builder.Services.AddScoped<IBenefitService, BenefitService>();
-    // builder.Services.AddScoped<IBenefitProvisionService, BenefitProvisionService>();
-    // builder.Services.AddScoped<IBenefitItemService, BenefitItemService>();
+builder.Services.AddAuthorization();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
@@ -84,6 +81,8 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapControllers();
 
 app.MapBlazorHub();
 
