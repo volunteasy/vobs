811a254c1d322f93c7ddab32b6c0323e6e59f11f
eakeur <igor.reisleandro@gmail.com>
2023-07-31 11:30:14 -0300

    feat: migrations

---
+++ src/Volunteasy.Api/Migrations/20230731142448_OptionalBeneficiaryFields.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Volunteasy.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class OptionalBeneficiaryFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Beneficiaries_Addresses_AddressId",
+                table: "Beneficiaries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Beneficiaries",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Beneficiaries",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AddressId",
+                table: "Beneficiaries",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Beneficiaries_Addresses_AddressId",
+                table: "Beneficiaries",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Beneficiaries_Addresses_AddressId",
+                table: "Beneficiaries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Beneficiaries",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Beneficiaries",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AddressId",
+                table: "Beneficiaries",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Beneficiaries_Addresses_AddressId",
+                table: "Beneficiaries",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
